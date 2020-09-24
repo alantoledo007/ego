4271fca7dcd17d1a2bf7b09252cbdcdf350d852f
alantoledo007 <alantoledo007@gmail.com>
2020-09-24 04:02:38 -0300

    install: react, react-redux, react-router-dom, redix-thunk

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Route>
+        
+      </Route> 
+    </React.Fragment>
+  );
+}
+
+export default App;
