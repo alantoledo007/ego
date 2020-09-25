c0e84b39bd5a6797b5d0fbc962695368ff4b2655
alantoledo007 <alantoledo007@gmail.com>
2020-09-25 11:43:06 -0300

    Updates

---
+++ precache-manifest.8562101c6cd2d23584227ebf02e7b9a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb354cefd35cb29649c8610a9be2106",
+    "url": "/ego/index.html"
+  },
+  {
+    "revision": "15e8f19e61891db9104c",
+    "url": "/ego/static/css/main.56366d05.chunk.css"
+  },
+  {
+    "revision": "9600cf20a77068ac159f",
+    "url": "/ego/static/js/2.92d47524.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ego/static/js/2.92d47524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e8f19e61891db9104c",
+    "url": "/ego/static/js/main.75cd85c5.chunk.js"
+  },
+  {
+    "revision": "05d7fcd04208c5a9dee4",
+    "url": "/ego/static/js/runtime-main.b8735f21.js"
+  }
+]);
